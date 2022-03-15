498c90a6ff4bd9240667e79659c04202d5d6ac28
Lackie <Justin.M.Lackie@Medstar.net>
2022-03-15 16:19:11 -0400

    loopin'

--- groupLoops.js
+++ groupLoops.js
@@ -1,8 +1,9 @@
-let arr = [45345,63456 ,6436 ,324, 4624 ,667467432 , 653566 ,245245]
+let sqlCol = 'yourSqlColumnName'
 let groups = [];
 let count = 0;
+let arr = [45345,63456 ,6436 ,324, 4624 ,667467432 , 653566 ,245245]
 
-function groupLoop(){
+function groupLoop(arr){
 for(i = 0; i < arr.length -1; i++){
      count++
      let group = {}
@@ -20,8 +21,30 @@ for(i = 0; i < arr.length -1; i++){
      }
       groups.push(group);
   }
-  console.log(groups) ;
+  //console.log(groups) ;
+  createWhereClause(sqlCol, groups)
 }
-console.log('well fuck');
 
-groupLoop();
+groupLoop(arr);
+
+
+
+function createWhereClause(colName,  groups) {
+
+  let where = ''
+  // loop through grouped elements and create string plugging in the array from each group in groups
+
+  for(group of groups){
+    for(const[ groupNumber, list ] of Object.entries(group)){
+     if(groupNumber < groups.length ){
+       where += ` ${colName} IN (${  list }) OR \n` 
+     }
+     else{
+       where +=  ` ${colName} IN (${ list }) `
+     }
+    }
+    
+  }
+  console.log(where);
+}
+
