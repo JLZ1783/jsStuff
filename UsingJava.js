66316de91e703d90ed868697876bdfad6d998dc1
JLZ1783 <50119881+JLZ1783@users.noreply.github.com>
2023-02-13 10:25:54 -0500

    Update UsingJava.js

--- UsingJava.js
+++ UsingJava.js
@@ -1,3 +1,7 @@
+const java = require('java');
+
+java.classpath.push('path/to/your/java-package.jar');
+
 const YourJavaClass = java.import('com.example.YourJavaClass');
 const instance = new YourJavaClass();
 instance.yourMethod();
